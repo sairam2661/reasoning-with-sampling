f72c331196ca8915db41a2bda75b5408b01a29c8
Aayush Karan <84253306+aakaran@users.noreply.github.com>
2025-10-15 21:15:46 -0400

    Refactor safe_grade to use parse_answer_gpqa

--- llm_experiments/eval_gpqa.py
+++ llm_experiments/eval_gpqa.py
@@ -7,7 +7,7 @@ from grader_utils.gpqa_grader import grade_answer, parse_answer_gpqa
 
 def safe_grade(ans, correct_ans):
     try:
-        return int(grade_answer(ans, correct_ans))
+        return int(grade_answer(parse_answer_gpqa(ans), correct_ans))
     except Exception:
         return 0
 
@@ -20,9 +20,9 @@ def eval_gpqa(fname):
     total = len(df)
 
     for i in range(total):
-        base_correct += safe_grade(parse_answer_gpqa(df["std_completion"][i]), df["correct_answer"][i])
-        temp_correct += safe_grade(parse_answer_gpqa(df["naive_completion"][i]), df["correct_answer"][i])
-        mcmc_correct += safe_grade(parse_answer_gpqa(df["mcmc_completion"][i][len(df["question"][i]):]), df["correct_answer"][i])
+        base_correct += safe_grade((df["std_completion"][i]), df["correct_answer"][i])
+        temp_correct += safe_grade((df["naive_completion"][i]), df["correct_answer"][i])
+        mcmc_correct += safe_grade((df["mcmc_completion"][i][len(df["question"][i]):]), df["correct_answer"][i])
 
     return base_correct, temp_correct, mcmc_correct, total
 
